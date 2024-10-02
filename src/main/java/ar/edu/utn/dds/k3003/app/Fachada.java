de18d749d575e609112320038d05a014422e2cbc
tomiballadares <tballadares@frba.utn.edu.ar>
2024-10-02 15:21:25 -0300

    metricsasd

--- src/main/java/ar/edu/utn/dds/k3003/app/Fachada.java
+++ src/main/java/ar/edu/utn/dds/k3003/app/Fachada.java
@@ -58,7 +58,7 @@ public class Fachada implements ar.edu.utn.dds.k3003.facades.FachadaHeladeras {
         heladera.setId(heladeraDTO.getId());
         //System.out.println(heladera.getId());
         this.repoHeladera.guardar(heladera);
-        heladerasCreadasCounter.count();
+        this.heladerasCreadasCounter.count();
         //System.out.println(this.repoHeladera.heladeras.stream().map(h->h.getId()).toList());
         return heladeraMapper.map(heladera);
     }
@@ -81,7 +81,7 @@ public class Fachada implements ar.edu.utn.dds.k3003.facades.FachadaHeladeras {
         repoHeladera.guardarVianda(vianda);
         repoHeladera.guardar(heladera);
         repoHeladera.actualizar(heladera);
-        viandasEnHeladerasCounter.count();
+        this.viandasEnHeladerasCounter.count();
     }
 
     @Override
@@ -112,7 +112,7 @@ public class Fachada implements ar.edu.utn.dds.k3003.facades.FachadaHeladeras {
         Heladera heladera = repoHeladera.findById(temperatura.getHeladeraId());
         heladera.agregarTemperatura(temperatura);
         repoHeladera.actualizar(heladera);
-        temperaturasRegistradasCounter.count();
+        this.temperaturasRegistradasCounter.count();
     }
 
     @Override
